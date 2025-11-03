9c313003c2acce93bb78f833f9a2303cc6ffcc0b
NewbieCoder76 <w.mxabanisi2003@gmail.com>
2025-11-03 14:51:26 +0200

    Tofu

---
+++ functions.js
@@ -0,0 +1,17 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// TODO: Create a function called add that takes two parameters
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// TODO: Call both functions and store their results in variables
+const greeting = greet("Will Mxabanisi. It is nice to see you");
+const sum = add(10, 12);
+
+// TODO: Print the results to the console   
+console.log(greeting);    
+console.log(sum);
